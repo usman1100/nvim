5172901d79aaf32ef6430d28da8b8a8afb20bf6b
usman1100 <usmanahmed1100@gmail.com>
2024-12-24 04:53:41 +0500

    custom telescope function

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,24 @@
+function is_a_git_repo()
+	local cwd = vim.fn.getcwd() -- Get the current working directory
+	local git_dir = cwd .. "/.git"
+
+	-- Check if the .git folder exists
+	if vim.fn.isdirectory(git_dir) == 1 then
+		print("You are in a Git repository.")
+		return true
+	else
+		print("Not a Git repository.")
+		return false
+	end
+end
+
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Find files' })
-vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = 'Find files (git)' })
+vim.keymap.set('n', '<leader>fa', builtin.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>ff', function()
+	local is_repo = is_a_git_repo()
+	if is_repo then
+		builtin.git_files()
+	else
+		builtin.find_files()
+	end
+end)

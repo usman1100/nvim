9aa5b3451cfbff82b90c2960266f26504c729eae
usman1100 <usmanahmed1100@gmail.com>
2024-12-26 09:31:10 +0500

    added some commments to remaps

--- lua/modules/remaps.lua
+++ lua/modules/remaps.lua
@@ -1,4 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+-- Go to the start of the line
 vim.keymap.set("n", "<leader>hh", '^')
+-- Go to the end of the line
 vim.keymap.set("n", "<leader>ll", '$')
